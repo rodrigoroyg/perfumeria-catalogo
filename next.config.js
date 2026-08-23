a0dda8e337417a44e6f907cf797da0c794ecad4c
rodrigoroyg <rodrigo92royg@gmail.com>
2026-08-23 13:10:37 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['rtchsualyoxvlxqbydhh.supabase.co', 'via.placeholder.com'],
+  },
+}
+
+module.exports = nextConfig
